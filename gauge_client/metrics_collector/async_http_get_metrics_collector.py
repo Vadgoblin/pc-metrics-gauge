eb82e4deaa25ca4076aaab5d80086bf2cc6b6262
vadgoblin@arch <vadgoblin@github.com>
2026-01-21 21:59:28 +0100

    remove dead code from metrics collector

--- gauge_client/metrics_collector/async_http_get_metrics_collector.py
+++ gauge_client/metrics_collector/async_http_get_metrics_collector.py
@@ -1,26 +1,6 @@
 import uaiohttpclient as aiohttp
 import json
 
-async def get_metrics(endpoint, interval):
-    url = endpoint
-    if interval:
-        url += f"?interval={interval}"
-
-    response = await aiohttp.request("GET", url)
-    
-    try:
-        response_data = await response.read()
-        decoded_response = (response_data).decode()
-        metrics = json.loads(decoded_response)
-        return metrics
-    finally:
-        await response.aclose()
-
-
-async def iter_metrics(endpoint, interval):
-    while True:
-        yield await get_metrics(endpoint, interval)
-
 
 class AsyncHttpGetMetricsCollector:
     def __init__(self, endpoint, interval):
--- gauge_client/metrics_collector/sync_http_get_metrics_collector.py
+++ gauge_client/metrics_collector/sync_http_get_metrics_collector.py
@@ -1,21 +1,4 @@
 import requests
-
-def get_metrics(endpoint, interval):
-    url = endpoint
-    if interval:
-        url += f"?interval={interval}"
-
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        raise Exception()
-    
-    return response.json()
-
-def iter_metrics(endpoint, interval):
-    while True:
-        yield get_metrics(endpoint, interval)
-        
         
 class SyncHttpGetMetricsCollector:
     def __init__(self, endpoint, interval):

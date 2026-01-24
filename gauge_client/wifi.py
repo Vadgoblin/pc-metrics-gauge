d53158baafe6c3a99f186b6c24376c0df1e3b6dc
vadgoblin@arch <vadgoblin@github.com>
2026-01-24 21:10:24 +0100

    improve wifi power managment handling

--- gauge_client/wifi.py
+++ gauge_client/wifi.py
@@ -6,10 +6,14 @@ def connect(ssid, password):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
-    wlan.config(pm=wlan.PM_POWERSAVE)
 
     while not wlan.isconnected():
         sleep(0.2)
+        
+    try:
+        wlan.config(pm=wlan.PM_POWERSAVE)
+    except:
+        print("failed to set power management profile for wifi")
 
     print("Successfully connected to the wifi")
     print(wlan.ifconfig())

98ecf0a46de7f3c8178ade6d98e9730fd34f391c
vadgoblin@arch <vadgoblin@github.com>
2026-02-05 21:45:07 +0100

    update code for soldered board

--- gauge_client/main.py
+++ gauge_client/main.py
@@ -8,8 +8,13 @@ from metrics_collector import AsyncHttpGetMetricsCollector, AsyncWebsocketMetric
 from gauge import SmoothGauge
 
 
-g1 = SmoothGauge(Pin(14), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
-g2 = SmoothGauge(Pin(15), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
+# pinout of my board is kinda funky
+# 3 2 0 4
+g1 = SmoothGauge(Pin(3), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
+g2 = SmoothGauge(Pin(2), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
+g3 = SmoothGauge(Pin(0), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
+g4 = SmoothGauge(Pin(4), config.DUTY_CYCLE_4_GAUGE_100_PERCENT)
+
 wifi.connect(config.SSID, config.PASSWORD)
 
 #metrics_collector = AsyncHttpGetMetricsCollector(config.GET_ENDPOINT, config.INTERVAL)
@@ -23,6 +28,8 @@ async def main():
             print(e)
             g1.set_value(0)
             g2.set_value(0)
+            g3.set_value(0)
+            g4.set_value(0)
             await asyncio.sleep(config.RETRY_INTERVAL)
             
 async def loop():
@@ -36,6 +43,8 @@ async def loop():
             
             
         g1.set_value(cpu_usage / 100)
-        g2.set_value(ram_usage / 100)
+        g2.set_value(cpu_temp / 100)
+        g3.set_value(ram_usage / 100)
+        g4.set_value(swap_usage / 100)
 
 asyncio.run(main())
